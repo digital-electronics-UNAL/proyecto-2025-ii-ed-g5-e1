3f52ea0814e84d1a822f0398b02d729270527002
aljio <alpulidos@unal.edu.co>
2025-12-01 15:19:24 -0500

    adit1

--- proyecto/top_top.v
+++ proyecto/top_top.v
@@ -4,19 +4,20 @@ input [3:0] columns;
 input [3:0] rows;
 input boton; //pulsador de adentro
 input reed; //sensor magnetico
-input pos_sel; //del servo
+output pos_sel; //del servo
 
-input   reset,
+input   reset,  //reset de solo el ultrasonido
 input   echo,        // Señal del sensor (ECHO)
 output reg  trigger, // Señal de disparo (TRIGGER)
 
 output buzz; //buzzer de alerta, debe ser un reg en su cajita
+
 output servo; //servomotor debe ser un reg en su cajita
 
 );
 
 wire nume;
-wire dista;
+wire [15:0] dista;
 
 ultrasonico_hcsr04 ultra(.clk(clk), .reset(reset), .echo(echo), .trigger(trigger), .distancia_cm(dista))
 
@@ -41,18 +42,21 @@ localparam STATE1 = 3'b001;
 localparam STATE2 = 3'b010;
 localparam STATE3 = 3'b011;
 localparam STATE4 = 3'b100;
-localparam STATE5 = 3'b101;
+localparam STATEP = 3'b101;
 localparam STATE6 = 3'b110;
 localparam STATE7 = 3'b111;
 
 reg [2:0] fsm_state;//el estado presente. el que va a saltar entre los valores de localparam
 reg [2:0] next_state;
 
+//wire [1:0] j;
 
 initial begin
     fsm_state <= IDLE;
     buzz <= 0
     pos_sel <= 0
+    //hacer un beginin para lo mostrado en la LCD
+    //j=2'b00
 end
 
 
@@ -79,6 +83,17 @@ always @(*) begin
 		STATE4: begin
 			next_state <= (reed == 0)? STATE4 : IDLE;
 		end
+        
+ //       STATE2:begin
+//			next_state <= (j==2'b10)? STATEP : STATE2; 
+ //       end
+
+
+
+
+//      STATEP: begin
+//			next_state <= (u)? STATEP : IDLE;
+//		end
         default: next_state = IDLE;
     endcase
 end
@@ -92,31 +107,39 @@ always @(posedge clk_16ms) begin
     end else begin
         case (next_state)
             IDLE: begin
-                command_counter <= 'b0;
-                data_counter <= 'b0;
-                rs <= 1'b0;
-                data  <= 'b0;
+                buzz <= 0
+                pos_sel <= 0
+                //lcd <= " "
             end
-            CONFIG_CMD1: begin
-			    rs <= 1'b0; 	
-                command_counter <= command_counter + 1;
-				data <= config_mem[command_counter];
+            STATE1: begin
+                buzz <= 0
+                pos_sel <= 0
+                //lcd <= "ingrese perfil seguido de *"
             end
-            WR_STATIC_TEXT_1L: begin
-                data_counter <= data_counter + 1;
-                rs <= 1'b1; 
-				data <= static_data_mem[data_counter];
+            STATE2: begin
+                buzz <= 0
+                pos_sel <= 0
+                //lcd <= "digite contraseña seguido de #"
+                //j=j + 2'b01
             end
-            CONFIG_CMD2: begin
-                data_counter <= 'b0;
-				rs <= 1'b0; 
-				data <= START_2LINE;
+            STATE3: begin
+                buzz <= 0
+                pos_sel <= 1
+                //lcd <= "Abierto"
             end
-			WR_STATIC_TEXT_2L: begin
-                data_counter <= data_counter + 1;
-                rs <= 1'b1; 
-				data <= static_data_mem[NUM_DATA_PERLINE + data_counter];
+			STATE4: begin
+                buzz <= 0
+                pos_sel <= 1
+                //lcd <= "Abierto"
             end
+	//		STATEP: begin
+     //           buzz <= 1
+    //            pos_sel <= 0
+   //             //lcd <= "Intruso"
+   //             j=2'b00
+  //          end
+
+
         endcase
     end
 end
