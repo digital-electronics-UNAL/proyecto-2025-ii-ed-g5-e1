6cef8b95c94f53b5223df900b81cbcced64c310c
aljio <alpulidos@unal.edu.co>
2025-12-09 13:52:22 -0500

    kjshfkjdh

--- proyecto/tecladoejtop.v
+++ proyecto/tecladoejtop.v
@@ -1,15 +1,14 @@
-module tecladoejtop(clk, columns, rows, nume);
+module tecladoejtop(clk, columns, nume);
 input clk;
 input [3:0] columns;
-input [3:0] rows;
-output reg [3:0] nume;
+output [3:0] nume;
 
 wire clk_1;
 wire [3:0] rows;
 
 
-div_f div(clk, clk_1);
-swept sp(clk_1, rows);
-compt comp(clk_1, columns, rows, nume);
+div_f div(.clk(clk), .clk_1(clk_1));
+swept sp(.clk_1(clk_1), .rows(rows));
+compt comp(.clk_1(clk_1), .columns(columns), .rows(rows), .num(nume));
 
 endmodule
