aa5f3e36ddffddb8d56ea87f7639466f2e6d4133
aljio <alpulidos@unal.edu.co>
2025-12-10 08:29:27 -0500

    conavancetopydeco

--- proyecto/codeahexa.v
+++ proyecto/codeahexa.v
@@ -1,7 +1,40 @@
-module code #(
-   
-)(
+module codahexa (
+    
+
     input [3:0] digdec,
-    output [3:0] dighex
+    output reg [6:0] dighex
 );
+
+always @ ( * ) begin
+     if (digdec == 4'b1111) dighex = 7'h30; // "0"
+     if (digdec == 4'b1110) dighex = 7'h31; // "1"  
+  	 if (digdec == 4'b1101) dighex = 7'h32; // "2"
+     if (digdec == 4'b1100) dighex = 7'h33; // "3"
+     if (digdec == 4'b1011) dighex = 7'h34; // "4"
+     if (digdec == 4'b1010) dighex = 7'h35; // "5"
+     if (digdec == 4'b1001) dighex = 7'h36; // "6"
+     if (digdec == 4'b1000) dighex = 7'h37; // "7"
+     if (digdec == 4'b0111) dighex = 7'h38; // "8"
+     if (digdec == 4'b0110) dighex = 7'h39; // "9"
+     if (digdec == 4'b0101) dighex = 7'h41; // "A"
+     if (digdec == 4'b0100) dighex = 7'h42; // "B"
+     if (digdec == 4'b0011) dighex = 7'h43; // "C"
+     if (digdec == 4'b0010) dighex = 7'h44; // "D"
+     if (digdec == 4'b0001) dighex = 7'h2A; // "*"
+     if (digdec == 4'b0000) dighex = 7'h23; // "#"
+     
+
+       
+    
+  end
+
+
+
+
+
+
+
+
+
+
 endmodule
